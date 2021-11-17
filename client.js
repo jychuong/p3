b61d481a4dccbbc13817a5779127e1e28ccea007
jychuong <jessicaychuong@gmail.com>
2021-11-17 04:06:55 -0800

    .

--- client.js
+++ client.js
@@ -1,62 +1,146 @@
-// Art 109 Three.js Demo Site
-// client.js
-// A basic three.js scene which displays and rotates a polygon with a wireframe
+// Import three.js core
+import * as THREE from "./build/three.module.js";
 
-// Extract globals from extrenal script
-const { THREE } = window;
+// Import add-ons for glTF models, orbit controls, and font loader
+import {
+  OrbitControls
+} from "./src/OrbitControls.js";
+import {
+  GLTFLoader
+} from "./src/GLTFLoader.js";
 
-// Create a scene
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  50,
-  500 / 500,
-  //window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-const renderer = new THREE.WebGLRenderer();
-renderer.setClearColor(0xdfdfdf);
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(500, 500);
-document.body.appendChild(renderer.domElement);
+let container, scene, camera, renderer, mesh, mesh2, mixer, controls, clock;
 
-// Add a polygon to the scene
-const geometry = new THREE.IcosahedronGeometry(1, 0);
-const material = new THREE.MeshStandardMaterial({ color: 0x2e8e39 });
-const poly = new THREE.Mesh(geometry, material);
-scene.add(poly);
 
-// add wireframe to shape
-const matLineBasic = new THREE.LineBasicMaterial({ color: 0x2e8e99 });
-const wireframe = new THREE.WireframeGeometry(geometry);
-const line = new THREE.LineSegments(wireframe, matLineBasic);
-line.material.depthTest = false;
-line.material.opacity = 0.9;
-line.material.transparent = false;
-scene.add(line);
+let ticker = 0;
 
-// Position our camera so we can see the shape
-camera.position.z = 5;
+// Call init and animate functions (defined below)
+init();
+animate();
 
-// Add a directional light to the scene
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
-scene.add(directionalLight);
+function init() {
 
-// Add an ambient light to the scene
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.95);
-scene.add(ambientLight);
+  //Identify div in HTML to place scene
+  container = document.getElementById("space");
 
-// Define and then call the render loop
-// Define
-function render() {
-  requestAnimationFrame(render);
+  //Crate clock for animation
+  clock = new THREE.Clock();
 
-  // Rotate our shape
-  poly.rotation.x += 0.005;
-  poly.rotation.y += 0.005;
-  line.rotation.x += 0.005;
-  line.rotation.y += 0.005;
-  renderer.render(scene, camera);
+  //Create scene
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor(0x7781A6);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, 900);
+  // Add scene to gltf.html
+  container.appendChild(renderer.domElement);
+
+  // Material to be added to preanimated model
+  var newMaterial = new THREE.MeshStandardMaterial({
+    color: 0x2E5939
+  });
+
+  // Load preanimated model, add material, and add it to the scene
+  const loader = new GLTFLoader().load(
+    "./assets/world.glb",
+    function(gltf) {
+      gltf.scene.traverse(function(child) {
+        if (child.isMesh) {
+          // child.material = newMaterial;
+          //makes colors more like the blender model
+          renderer.outputEncoding = THREE.sRGBEncoding;
+        }
+      });
+      // set position and scale
+      mesh = gltf.scene;
+      mesh.position.set(0, 0.5, 0);
+      mesh.rotation.set(0, 0, 0);
+      mesh.scale.set(1, 1, 1);
+      // Add model to scene
+      scene.add(mesh);
+      //Check for and play animation frames
+      mixer = new THREE.AnimationMixer(mesh);
+      gltf.animations.forEach((clip) => {
+        mixer.clipAction(clip).play();
+      });
+
+    },
+    undefined,
+    function(error) {
+      console.error(error);
+    }
+  );
+
+
+  // Add Orbit Controls
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.minDistance = 3;
+  controls.maxDistance = 12;
+  controls.target.set(0, 0, 0);
+  controls.autoRotate = true;
+  controls.autoRotateSpeed = 3;
+  controls.maxPolarAngle = Math.PI / 1.5;
+  controls.minPolarAngle = Math.PI / 2;
+
+
+  // Position our camera so we can see the shape
+  camera.position.y = 7;
+
+  // Add a directional light to the scene
+  const light = new THREE.DirectionalLight( 0xDFD6FF, 0.6 );
+  light.position.set( 0, 10, 10 );
+  light.castShadow = true;
+  scene.add( light );
+
+  //Set up shadow properties for the light
+  light.shadow.mapSize.width = 512; // default
+  light.shadow.mapSize.height = 512; // default
+  light.shadow.camera.near = 0.5; // default
+  light.shadow.camera.far = 500; // default
+
+  const light2 = new THREE.DirectionalLight( 0xDFD6FF, 0.4, 100 );
+  light2.position.set( 0, 2, 10 );
+  light2.castShadow = true;
+  scene.add( light2 );
+
+
+  // Add an ambient light to the scene
+  const ambientLight = new THREE.AmbientLight(0xAEADFF, 0.9);
+  scene.add(ambientLight);
+
 }
-// Call
-render();
+
+
+// Define animate loop
+function animate() {
+  controls.update();
+  requestAnimationFrame(animate);
+  var delta = clock.getDelta();
+  if (mixer) mixer.update(delta);
+  render();
+}
+
+// Define the render loop
+function render() {
+  renderer.render(scene, camera);
+  manualAnimation();
+}
+
+
+
+// Respond to Window Resizing
+window.addEventListener("resize", onWindowResize);
+
+// Window resizing function
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+  render();
+}
